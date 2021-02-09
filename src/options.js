ef45f02aab7eb7bb742f2f04ab4a8f0674e04424
ao-spirogyra <lily.spirogyra@gmail.com>
2021-02-09 17:04:29 +0900

    install eslint

---
+++ src/options.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form');
+const saveButton = document.getElementById('save');
+// localStorage.removeItem("exception-list")
+if (!localStorage.getItem('exception-list')) {
+  localStorage.setItem('exception-list', JSON.stringify([]));
+} else {
+}
+const saveForm = () => {
+  const newValue = form.value;
+  if (newValue !== '') {
+    const exceptionList = JSON.parse(localStorage.getItem('exception-list'));
+    exceptionList.push(newValue);
+    localStorage.setItem('exception-list', JSON.stringify(exceptionList));
+  }
+};
+saveButton.addEventListener('click', saveForm);
+const ul = document.createElement('ul');
+JSON.parse(localStorage.getItem('exception-list')).forEach((item) => {
+  let li = document.createElement('li');
+  li.textContent = item;
+  ul.appendChild(li);
+});
+document.getElementById('exceptions').appendChild(ul);
